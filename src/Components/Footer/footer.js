ce7929bf3bd01698cf0dc37d7c9bca881b6ec0a2
Vaibhav <vaibhavchauhan2410@gmail.com>
2022-01-11 16:22:51 +0530

    header added

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div>
+            <p>All rights of clubsabha 2022 is reserved for clubsabha's team</p>
+            <div>
+                Hello there
+             </div>
+        </div>
+
+
+    )
+}
+
+export default footer
